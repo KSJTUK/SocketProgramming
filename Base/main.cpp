abb42c05ceffef7fa5ae274d2e7c746ce82e3f7e
KSJTUK <lastbabo2480@tukorea.ac.kr>
2024-09-04 21:17:16 +0900

    기본적인 socket을 이용한 서버 작성
    Base에서는 각 socket 함수에 대해 나름대로 알아본 내용들을 주석으로 적음,
    이후 코드에서는 이러한 내용을 적지는 않을 예정

--- Base/main.cpp
+++ Base/main.cpp
@@ -1,20 +1,95 @@
-﻿// Base.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 
-#include <iostream>
+// 소켓 프로그래밍을 위한 헤더
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// ws2_32 라이브러리, ws2_32.dll과 링크
+#pragma comment(lib, "ws2_32.lib")
+
+// 사용할 포트 번호 정의, 포트 번호는 16비트 정수를 이용 (unsigned short)
+constexpr unsigned short PORT_NUMBER = 7777;
+// 테스트용으로 보낼 문자열의 최대 길이 정의
+constexpr unsigned short SEND_SIZE = 1024;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // WSAStartup 윈도우 소켓 초기화를 위한 변수, 사용되는 일은 적음
+    WSAData wsaData;
+
+    if (0 != ::WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+
+    // socket 생성
+    // AF_INET -> IPv4, SOCK_STREAM -> TCP 소켓, 프로토콜 0 -> 자동으로 결정
+    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == socket) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+
+    // sockaddr_in 구조체를 이용해 ip 주소, port 번호를 소켓에 바인딩
+    // address.sin_addr.s_addr = ::inet_addr("127.0.0.1"); inet_addr은 구버전 함수이기에 컴파일러에서 경고발생
+    sockaddr_in address;
+    std::memset(&address, 0, sizeof(sockaddr_in));
+    address.sin_family = AF_INET;                       // ip 프로토콜 (ip 버전)
+    address.sin_port = ::htons(PORT_NUMBER);            // 포트 번호
+    int ptonResult = ::inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr); // ip 주소
+    if (0 == ptonResult) {
+        std::cout << "IP 주소 형식이 지정한 IP 버전과 맞지 않음\n";
+        return EXIT_FAILURE;
+    }
+    else if (-1 == ptonResult) {
+        // 에러 핸들링
+        return EXIT_FAILURE;
+    }
+    
+    // 위 코드에서 입력한 내용을 소켓에 바인딩
+    if (SOCKET_ERROR == ::bind(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in))) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+
+    // 소켓을 listen 상태로 변경 -> 수신 대기 상태로 변경
+    // 소켓과 backlog를 받는다, 
+    // backlog -> 해당 소켓에 PENDING, 해당 소켓에 연결되려고 기다리는 소켓의 최대 숫자라고 생각
+    if (SOCKET_ERROR == listen(socket, 10)) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+
+    sockaddr_in clientAddr;
+    int clientAddrLen{ };
+    // 실제 연결 대기중인 소켓과 연결, 결과 값으로 연결된 소켓을 반환
+    SOCKET clientSocket = ::accept(socket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
+    if (INVALID_SOCKET == clientSocket) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+    /* ------------------------ 여기까지 무사히 실행 되었다면 연결완료 ------------------------ */
+
+    // 테스트용 send 잘 가는지는 클라이언트 부분을 만들어서 확인!
+    // 주의! 처음에 만든 소켓은 서버에서 클라이언트와 연결을 위한 소켓임, 보낼때는 accept에서 반환된 소켓을 이용
+    char sendData[SEND_SIZE]{ "Test Send Data To Client" };
+    if (SOCKET_ERROR == ::send(clientSocket, sendData, SEND_SIZE, 0)) {
+        // 에러 핸들링 추가
+        return EXIT_FAILURE;
+    }
+
+    // 생성된 소켓 
+    if (SOCKET_ERROR == ::closesocket(socket)) {
+        // 에러 핸들링
+        return EXIT_FAILURE;
+    }
+
+    // ws2_32.dll의 사용을 종료하는 함수
+    // MSDN에서는 WSAStartup과 쌍을 이루어 호출하는 것을 권장하고 있음.
+    // 자세한 설명은 아래 링크 참고
+    // https://learn.microsoft.com/ko-kr/windows/win32/api/winsock2/nf-winsock2-wsacleanup
+    if (0 != ::WSACleanup()) {
+        // 에러 핸들링
+        return EXIT_FAILURE;
+    }
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
