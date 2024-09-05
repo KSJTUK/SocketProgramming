441435fc0040ff0a9da7a8903597421cb6f6d422
KSJTUK <lastbabo2480@tukorea.ac.kr>
2024-09-05 21:56:21 +0900

    IPv4, IPv6 두 버전으로 함수를 나눔, Send&Recv 중복 부분 모듈화

--- TCPServer/Base/main.cpp
+++ TCPServer/Base/main.cpp
@@ -25,14 +25,14 @@ void HandleSocketError(const char* cause = nullptr)
 {
     int socketErrorCode = ::WSAGetLastError();
     
-    wchar_t errorMessageBuf[BUFSIZ]{ };
-    ::FormatMessage(
-        FORMAT_MESSAGE_FROM_SYSTEM,
+    void* errorMessage = nullptr;
+    ::FormatMessageA(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
         nullptr,
         socketErrorCode,
-        MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT),
-        errorMessageBuf,
-        BUFSIZ,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        reinterpret_cast<char*>(&errorMessage),
+        0,
         nullptr
     );
 
@@ -41,62 +41,41 @@ void HandleSocketError(const char* cause = nullptr)
     }
 
     std::cout << "Last Error Code: " << socketErrorCode << "\n";
-    std::wcout << L"Formatted Error Message:" << errorMessageBuf << "\n";
+    std::cout << "Formatted Error Message:" << reinterpret_cast<char*>(errorMessage) << "\n";
+
+    LocalFree(errorMessage);
 }
 
-int main()
+// 중복될 수 있는 WSAStartup, WSACleanup은 따로 함수를 만들어 관리
+void WinSockStart()
 {
     // WSAStartup 윈도우 소켓 초기화를 위한 변수, 사용되는 일은 적음
     WSAData wsaData;
 
     if (0 != ::WSAStartup(MAKEWORD(2, 2), &wsaData)) {
         HandleSocketError();
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
+}
 
-    // socket 생성
-    // AF_INET -> IPv4, SOCK_STREAM -> TCP 소켓, 프로토콜 0 -> 자동으로 결정
-    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
-    if (INVALID_SOCKET == socket) {
-        HandleSocketError();
-        return EXIT_FAILURE;
-    }
+void WinSockClean()
+{
+	// ws2_32.dll의 사용을 종료하는 함수
+    // MSDN에서는 WSAStartup과 쌍을 이루어 호출하는 것을 권장하고 있음.
+    // 자세한 설명은 아래 링크 참고
+    // https://learn.microsoft.com/ko-kr/windows/win32/api/winsock2/nf-winsock2-wsacleanup
+	if (0 != ::WSACleanup()) {
+		HandleSocketError();
+		::exit(EXIT_FAILURE);
+	}
+}
 
-    // sockaddr_in 구조체를 이용해 ip 주소, port 번호를 소켓에 바인딩
-    sockaddr_in address;
-    std::memset(&address, 0, sizeof(sockaddr_in));
-    address.sin_family = AF_INET;                       // ip 프로토콜 (ip 버전)
-    address.sin_port = ::htons(PORT_NUMBER);            // 포트 번호
-    address.sin_addr.s_addr = ::htonl(INADDR_ANY);      // 서버로 들어오는 모든 ip를 받도록 설정
-    
-    // 위 코드에서 입력한 내용을 소켓에 바인딩
-    if (SOCKET_ERROR == ::bind(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in))) {
-        HandleSocketError();
-        return EXIT_FAILURE;
-    }
-
-    // 소켓을 listen 상태로 변경 -> 수신 대기 상태로 변경
-    // 소켓과 backlog를 받는다, 
-    // backlog -> 해당 소켓에 PENDING, 해당 소켓에 연결되려고 기다리는 소켓의 최대 숫자라고 생각
-    if (SOCKET_ERROR == listen(socket, 10)) {
-        HandleSocketError();
-        return EXIT_FAILURE;
-    }
-
-    sockaddr_in clientAddr;
-    int clientAddrLen{ sizeof(sockaddr_in) };
-    // 실제 연결 대기중인 소켓과 연결, 결과 값으로 소켓을 반환
-    SOCKET clientSocket = ::accept(socket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
-    if (INVALID_SOCKET == clientSocket) {
-        HandleSocketError();
-        return EXIT_FAILURE;
-    }
-    std::cout << "연결 성공!\n";
-    /* ------------------------ 여기까지 무사히 실행 되었다면 연결완료 ------------------------ */
-
+// 두 IP버전 모두 같은 로직으로 Send, Recv 하므로 Socket만 인자로 받아 함수로 처리
+void SendAndRecv(SOCKET clientSocket)
+{
     // 테스트용 send 잘 가는지는 클라이언트 부분을 만들어서 확인!
     // 주의! 처음에 만든 소켓은 서버에서 클라이언트와 연결을 위한 소켓임, accept를 위해 생성한 소켓을 이용
-    
+
     std::string sendData{ };
     char recvData[RECV_SIZE]{ };
     while (true) {
@@ -112,7 +91,7 @@ int main()
         if (SEND_SIZE <= dataSize) {
             std::cout << "입력제한 1023자를 넘어섰습니다. 다시 입력해주세요.\n";
             std::cin.clear();
-            std::cin.ignore(std::numeric_limits<int>::max() , '\n');
+            std::cin.ignore(std::numeric_limits<int>::max(), '\n');
             continue;
         }
 
@@ -127,7 +106,7 @@ int main()
         // 문자열 보내기
         if (SOCKET_ERROR == ::send(clientSocket, sendData.c_str(), dataSize, 0)) {
             HandleSocketError();
-            return EXIT_FAILURE;
+            ::exit(EXIT_FAILURE);
         }
 
         // 보냈으면 받기
@@ -155,6 +134,55 @@ int main()
 
         // 다 받으면 다시 반복
     }
+}
+
+// IPv4를 사용하는 서버
+void TCPServerIPv4()
+{
+    // socket 생성
+    // AF_INET -> IPv4, SOCK_STREAM -> TCP 소켓, 프로토콜 0 -> 자동으로 결정
+    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == socket) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    // sockaddr_in 구조체를 이용해 ip 주소, port 번호를 소켓에 바인딩
+    sockaddr_in address;
+    std::memset(&address, 0, sizeof(sockaddr_in));
+    address.sin_family = AF_INET;                       // ip 프로토콜 (ip 버전)
+    address.sin_port = ::htons(PORT_NUMBER);            // 포트 번호
+    address.sin_addr.s_addr = ::htonl(INADDR_ANY);      // 서버로 들어오는 모든 ip를 받도록 설정
+
+    // 위 코드에서 입력한 내용을 소켓에 바인딩
+    if (SOCKET_ERROR == ::bind(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in))) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    // 소켓을 listen 상태로 변경 -> 수신 대기 상태로 변경
+    // 소켓과 backlog를 받는다, 
+    // backlog -> 해당 소켓에 PENDING, 해당 소켓에 연결되려고 기다리는 소켓의 최대 숫자라고 생각
+    if (SOCKET_ERROR == listen(socket, SOMAXCONN)) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in clientAddr;
+    int clientAddrLen{ sizeof(sockaddr_in) };
+    // 실제 연결 대기중인 소켓과 연결, 결과 값으로 소켓을 반환
+    SOCKET clientSocket = ::accept(socket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
+    if (INVALID_SOCKET == clientSocket) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    char ipv4Address[INET_ADDRSTRLEN]{ };
+    ::inet_ntop(AF_INET, &clientAddr.sin_addr, ipv4Address, INET_ADDRSTRLEN);
+    std::cout << "[클라이언트 접속] IPv6: " << ipv4Address << ", PORT: " << ::ntohs(clientAddr.sin_port) << "\n";
+    /* ------------------------ 여기까지 무사히 실행 되었다면 연결완료 ------------------------ */
+
+    SendAndRecv(clientSocket);
 
     // 소켓 연결의 종료를 알리는 함수, 소켓 뒤의 파라미터는 어떤 작업(SEND?, RECV?, BOTH?)을 중단할 것인지 설정
     ::shutdown(clientSocket, SD_BOTH);
@@ -162,18 +190,75 @@ int main()
     // 생성된 소켓 닫기
     if (SOCKET_ERROR == ::closesocket(socket)) {
         HandleSocketError();
-        return EXIT_FAILURE;
+        ::exit(EXIT_FAILURE);
     }
+}
 
-    // ws2_32.dll의 사용을 종료하는 함수
-    // MSDN에서는 WSAStartup과 쌍을 이루어 호출하는 것을 권장하고 있음.
-    // 자세한 설명은 아래 링크 참고
-    // https://learn.microsoft.com/ko-kr/windows/win32/api/winsock2/nf-winsock2-wsacleanup
-    if (0 != ::WSACleanup()) {
+// IPv6를 사용하는 서버
+void TCPServerIPv6()
+{
+    // socket 생성
+    // AF_INET -> IPv4, SOCK_STREAM -> TCP 소켓, 프로토콜 0 -> 자동으로 결정
+    SOCKET socket = ::socket(AF_INET6, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == socket) {
         HandleSocketError();
-        return EXIT_FAILURE;
+        ::exit(EXIT_FAILURE);
     }
 
+    // sockaddr_in 구조체를 이용해 ip 주소, port 번호를 소켓에 바인딩
+    sockaddr_in6 address;
+    std::memset(&address, 0, sizeof(sockaddr_in6));
+    address.sin6_family = AF_INET6;                       // ip 프로토콜 (ip 버전)
+    address.sin6_port = ::htons(PORT_NUMBER);             // 포트 번호
+    address.sin6_addr = in6addr_any;                      // 서버로 들어오는 모든 ip를 받도록 설정
+
+    // 위 코드에서 입력한 내용을 소켓에 바인딩
+    if (SOCKET_ERROR == ::bind(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in6))) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    // 소켓을 listen 상태로 변경 -> 수신 대기 상태로 변경
+    // 소켓과 backlog를 받는다, 
+    // backlog -> 해당 소켓에 PENDING, 해당 소켓에 연결되려고 기다리는 소켓의 최대 숫자라고 생각
+    if (SOCKET_ERROR == listen(socket, SOMAXCONN)) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in6 clientAddr;
+    int clientAddrLen{ sizeof(sockaddr_in6) };
+    // 실제 연결 대기중인 소켓과 연결, 결과 값으로 소켓을 반환
+    SOCKET clientSocket = ::accept(socket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
+    if (INVALID_SOCKET == clientSocket) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+    
+    char ipv6Address[INET6_ADDRSTRLEN]{ };
+    ::inet_ntop(AF_INET6, &clientAddr.sin6_addr, ipv6Address, INET6_ADDRSTRLEN);
+    std::cout << "[클라이언트 접속] IPv6: " << ipv6Address << ", PORT: " << ::ntohs(clientAddr.sin6_port) << "\n";
+    /* ------------------------ 여기까지 무사히 실행 되었다면 연결완료 ------------------------ */
+
+    SendAndRecv(clientSocket);
+
+    // 소켓 연결의 종료를 알리는 함수, 소켓 뒤의 파라미터는 어떤 작업(SEND?, RECV?, BOTH?)을 중단할 것인지 설정
+    ::shutdown(clientSocket, SD_BOTH);
+
+    // 생성된 소켓 닫기
+    if (SOCKET_ERROR == ::closesocket(socket)) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+}
+
+int main()
+{
+    WinSockStart();
+
+    TCPServerIPv6();
+
+    WinSockClean();
     // 시작 프로젝트를 여러개로 지정했을 때 바로 끝나지 않게 하기위해 작성
     char ch{ };
     std::cout << "끝내려면 엔터키를 눌러주세요.";
--- TCPServer/ConsoleClient/baseClient.cpp
+++ TCPServer/ConsoleClient/baseClient.cpp
@@ -19,14 +19,14 @@ void HandleSocketError(const char* cause = nullptr)
 {
     int socketErrorCode = ::WSAGetLastError();
 
-    wchar_t errorMessageBuf[BUFSIZ]{ };
-    ::FormatMessage(
-        FORMAT_MESSAGE_FROM_SYSTEM,
+    void* errorMessage = nullptr;
+    ::FormatMessageA(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
         nullptr,
         socketErrorCode,
-        MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT),
-        errorMessageBuf,
-        BUFSIZ,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        reinterpret_cast<char*>(&errorMessage),
+        0,
         nullptr
     );
 
@@ -35,46 +35,38 @@ void HandleSocketError(const char* cause = nullptr)
     }
 
     std::cout << "Last Error Code: " << socketErrorCode << "\n";
-    std::wcout << L"Formatted Error Message:" << errorMessageBuf << "\n";
+    std::cout << "Formatted Error Message:" << reinterpret_cast<char*>(errorMessage) << "\n";
+
+    LocalFree(errorMessage);
 }
 
-int main()
+// 중복될 수 있는 WSAStartup, WSACleanup은 따로 함수를 만들어 관리
+void WinSockStart()
 {
+    // WSAStartup 윈도우 소켓 초기화를 위한 변수, 사용되는 일은 적음
     WSAData wsaData;
+
     if (0 != ::WSAStartup(MAKEWORD(2, 2), &wsaData)) {
         HandleSocketError();
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
+}
 
-    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
-    if (INVALID_SOCKET == socket) {
-        // 에러 핸들링 추가
-        return EXIT_FAILURE;
-    }
-
-    sockaddr_in address;
-    std::memset(&address, 0, sizeof(sockaddr_in));
-    address.sin_family = AF_INET;
-    address.sin_port = ::htons(PORT_NUMBER);
-    // address.sin_addr.s_addr = ::inet_addr("127.0.0.1"); inet_addr은 구버전 함수이기에 컴파일러에서 경고발생
-    int ptonResult = ::inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr); // ip 주소
-    if (0 == ptonResult) {
-        std::cout << "IP 주소 형식이 지정한 IP 버전과 맞지 않음\n";
-        return EXIT_FAILURE;
-    }
-    else if (-1 == ptonResult) {
+void WinSockClean()
+{
+    // ws2_32.dll의 사용을 종료하는 함수
+    // MSDN에서는 WSAStartup과 쌍을 이루어 호출하는 것을 권장하고 있음.
+    // 자세한 설명은 아래 링크 참고
+    // https://learn.microsoft.com/ko-kr/windows/win32/api/winsock2/nf-winsock2-wsacleanup
+    if (0 != ::WSACleanup()) {
         HandleSocketError();
-        return EXIT_FAILURE;
+        ::exit(EXIT_FAILURE);
     }
+}
 
-    // 해당 ip주소, 포트번호를 이용하여 실제로 서버와 연결하는 부분
-    if (SOCKET_ERROR == ::connect(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in))) {
-        HandleSocketError();
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "연결 성공!\n";
-
+// 
+void RecvAndSend(SOCKET socket)
+{
     std::string sendData{ };
     char recvData[RECV_SIZE]{ };
     while (true) {
@@ -124,16 +116,96 @@ int main()
         // 문자열 보내기
         if (SOCKET_ERROR == ::send(socket, sendData.c_str(), dataSize, 0)) {
             HandleSocketError();
-            return EXIT_FAILURE;
+            ::exit(EXIT_FAILURE);
         }
         std::cout << "보내기 완료.\n\n";
         std::cout << "----------------------------------------\n\n";
 
         // 다 받으면 다시 반복
     }
+}
+
+// IPv4를 사용하는 TCP 클라이언트
+void TCPClientIPv4()
+{
+    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == socket) {
+        // 에러 핸들링 추가
+        ::exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in address;
+    std::memset(&address, 0, sizeof(sockaddr_in));
+    address.sin_family = AF_INET;
+    address.sin_port = ::htons(PORT_NUMBER);
+    // address.sin_addr.s_addr = ::inet_addr("127.0.0.1"); inet_addr은 구버전 함수이기에 컴파일러에서 경고발생
+    int ptonResult = ::inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr); // ip 주소
+    if (0 == ptonResult) {
+        std::cout << "IP 주소 형식이 지정한 IP 버전과 맞지 않음\n";
+        ::exit(EXIT_FAILURE);
+    }
+    else if (-1 == ptonResult) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    // 해당 ip주소, 포트번호를 이용하여 실제로 서버와 연결하는 부분
+    if (SOCKET_ERROR == ::connect(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in))) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    std::cout << "연결 성공!\n";
+
+    RecvAndSend(socket);
 
     ::closesocket(socket);
-    ::WSACleanup();
+}
+
+// IPv6를 사용하는 TCP 클라이언트
+void TCPClientIPv6()
+{
+    SOCKET socket = ::socket(AF_INET6, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == socket) {
+        // 에러 핸들링 추가
+        ::exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in6 address;
+    std::memset(&address, 0, sizeof(sockaddr_in6));
+    address.sin6_family = AF_INET6;
+    address.sin6_port = ::htons(PORT_NUMBER);
+    // address.sin_addr.s_addr = ::inet_addr("127.0.0.1"); inet_addr은 구버전 함수이기에 컴파일러에서 경고발생
+    int ptonResult = ::inet_pton(AF_INET6, "0:0:0:0:0:0:0:1", &address.sin6_addr); // ip 주소
+    if (0 == ptonResult) {
+        std::cout << "IP 주소 형식이 지정한 IP 버전과 맞지 않음\n";
+        ::exit(EXIT_FAILURE);
+    }
+    else if (-1 == ptonResult) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    // 해당 ip주소, 포트번호를 이용하여 실제로 서버와 연결하는 부분
+    if (SOCKET_ERROR == ::connect(socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_in6))) {
+        HandleSocketError();
+        ::exit(EXIT_FAILURE);
+    }
+
+    std::cout << "연결 성공!\n";
+
+    RecvAndSend(socket);
+
+    ::closesocket(socket);
+}
+
+int main()
+{
+    WinSockStart();
+
+    TCPClientIPv6();
+
+    WinSockClean();
 
     // 시작 프로젝트를 여러개로 지정했을 때 바로 끝나지 않게 하기위해 작성
     char ch{ };
